4641e8d366695c8736e51c8fe7953b6c1a84d2bd
artem alexeenko <artyom.alexeyenko@gmail.com>
2025-03-29 12:17:27 +0500

    configure product context, get products from fakestore api

---
+++ src/contexts/ProductContext.jsx
@@ -0,0 +1,25 @@
+import React, {createContext, useState, useEffect} from 'react';
+
+export const ProductContext = createContext();
+
+const ProductProvider = ({children}) => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      fetch('https://fakestoreapi.com/products')
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+          setProducts(data);
+        });
+    };
+    fetchProducts();
+  }, [])
+  return (
+    <ProductContext.Provider value={{products}}>
+      {children}
+    </ProductContext.Provider>
+  )
+}
+
+export default ProductProvider
